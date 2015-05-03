748f9aaf1ea3f328aa092513ad56e4ef89916695
Adam Yowanda <yowanda6361@gmail.com>
2015-05-03 16:03:47 +0700

    Added logo panjang

--- application/views/v_navbar.php
+++ application/views/v_navbar.php
@@ -10,7 +10,7 @@
                 </button>
                 
                 <a class="navbar-brand" href="<?php echo site_url(); ?>">
-                    <img class="brand" src="<?php echo base_url('assets/images/logofull.jpg'); ?>" alt="SKP">
+                    <img class="brand" src="<?php echo base_url('assets/images/logopanjang.jpg'); ?>" alt="SKP">
                 </a>
             </div>
 
@@ -34,10 +34,10 @@
         $(window).scroll(function () {
             if ($(document).scrollTop() > 50) {
                 $('nav').addClass('shrink');
-				$('.brand').attr('src','<?php echo base_url('assets/images/logo.png'); ?>');
+				$('.brand').attr('src','<?php echo base_url('assets/images/logofull.jpg'); ?>');
             } else {
                 $('nav').removeClass('shrink');
-				$('.brand').attr('src','<?php echo base_url('assets/images/logofull.jpg'); ?>');
+				$('.brand').attr('src','<?php echo base_url('assets/images/logopanjang.jpg'); ?>');
             }
         });
 
